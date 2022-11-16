39741ab6722e1bbd92e7349aa7fd0023341eb7ae
Mo Xiaoming <2188767+mo-xiaoming@users.noreply.github.com>
2022-11-16 21:04:21 +0800

    remove llvm, and refactoring code

---
+++ src/utility.rs
@@ -0,0 +1,71 @@
+#![allow(dead_code)]
+
+#[cfg(test)]
+pub(crate) mod traits {
+    /// have everything a value type should have
+    pub(crate) fn is_small_value_struct<T>(_: &T)
+    where
+        T: Sync
+            + Send
+            + Copy
+            + Clone
+            + Default
+            + std::fmt::Debug
+            + std::hash::Hash
+            + PartialEq
+            + Eq
+            + PartialOrd
+            + Ord,
+    {
+    }
+    // like `is_small_value_struct`, but too big to be copied around
+    pub(crate) fn is_big_value_struct<T>(_: &T)
+    where
+        T: Sync
+            + Send
+            + Clone
+            + Default
+            + std::fmt::Debug
+            + std::hash::Hash
+            + PartialEq
+            + Eq
+            + PartialOrd
+            + Ord,
+    {
+    }
+    /// have everything a value type should have
+    /// short of `Default` compares to `is_small_value_struct`
+    pub(crate) fn is_small_value_enum<T>(_: &T)
+    where
+        T: Sync
+            + Send
+            + Copy
+            + Clone
+            + std::fmt::Debug
+            + std::hash::Hash
+            + PartialEq
+            + Eq
+            + PartialOrd
+            + Ord,
+    {
+    }
+    // like `is_small_value_enum`, but too big to be copied around
+    pub(crate) fn is_big_value_enum<T>(_: &T)
+    where
+        T: Sync
+            + Send
+            + Clone
+            + std::fmt::Debug
+            + std::hash::Hash
+            + PartialEq
+            + Eq
+            + PartialOrd
+            + Ord,
+    {
+    }
+    pub(crate) fn is_default_debug<T>(_: &T)
+    where
+        T: Default + std::fmt::Debug,
+    {
+    }
+}
---
+++ tests/artifacts/print_0.bf
@@ -0,0 +1,9 @@
+PRINT THE NUMBERS 1 TO 5
+++++++++ ++++++++ ++++++++ ++++++++ ++++++++ ++++++++ ASCII(48) '0'
+>+++++ COUNTER 5
+[
+    <  CELL 0
+    +. INC AND PRINT
+    >  CELL 1
+    -  DEC COUNTER
+]
